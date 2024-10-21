325ba79461c7aa3319f85ff246520e5e382911b2
Thilini Nisansala <thilininisansala953@gmail.com>
2024-10-21 20:02:54 +0530

    implimenting dotenv

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,24 +1,15 @@
 import express from "express";
-import {createCategory, deleteCategory, getCategory, getCategoryByName} from "../controllers/categoryController.js";
+import {createCategory, deleteCategory, getCategory, getCategoryByName, updateCategory} from "../controllers/categoryController.js";
 const categoryRouter = express. Router();
 
 categoryRouter.post("/", createCategory)
 
 categoryRouter.delete("/:name",deleteCategory)
 
-
-
-
-
-
-categoryRouter.get("/searchByPrice", (req,res)=>{
-  res.json({
-    message : "searchByPrice"
-  })
-})
-
 categoryRouter.get("/:name",getCategoryByName)
 
 categoryRouter.get("/",getCategory);
+
+categoryRouter.put("/:name",updateCategory);
 ;
 export default categoryRouter;
